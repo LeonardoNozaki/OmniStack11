d43ed0a39d7cda4d5f2f5c149c5e06aed465650c
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-04-10 12:28:34 -0300

    Alteracoes para deploy frontend

--- backend/tests/unit/generateUniqueId.spec.js
+++
@@ -1,9 +0,0 @@
-const generateUniqueId = require('../../src/utils/generatedUniqueId');
-
-describe('Generate Unique ID', () => {
-  it('should generate an unique ID', () => {
-    const id = generateUniqueId();
-
-    expect(id).toHaveLength(8)
-  });
-});
