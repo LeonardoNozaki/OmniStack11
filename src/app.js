1aa4468a84d00d3aaceb923d82a5ddc7f10ff1ce
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-04-10 12:19:39 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://bethehero-front.herokuapp.com' }));
 app.use(express.json());
 app.use(routes);
 app.use(errors());
